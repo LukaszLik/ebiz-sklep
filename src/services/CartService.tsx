8d513c5b6d3dc8ddf6744a914b374c3fbd5f64c8
LukaszLik <lik.lukasz2@gmail.com>
2021-09-03 21:18:02 +0200

    change api path to azure cloud app

--- src/services/CartService.tsx
+++ src/services/CartService.tsx
@@ -2,16 +2,16 @@ import axios from "axios";
 
 class CartService{
     async getCart(){
-        return axios.get("http://https://sklep-back2.azurewebsites.net/api/cartItems");
+        return axios.get("https://https://sklep-back2.azurewebsites.net/api/cartItems");
     }
 
     async addToCart(){
-        axios.post("http://https://sklep-back2.azurewebsites.net/api/")
+        axios.post("https://https://sklep-back2.azurewebsites.net/api/")
     }
 
     async buyProducts(userId: number, price: number){
         // /api/addOrder
-        axios.post("http://https://sklep-back2.azurewebsites.net/api/addOrder", {
+        axios.post("https://https://sklep-back2.azurewebsites.net/api/addOrder", {
             userId, price
         });
     }
